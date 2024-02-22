52d5ba6c73de09b1fac694f8bb106191f589c758
Salaheddine-ouikene <59568245+Salaheddine-ouikene@users.noreply.github.com>
2024-02-22 01:25:56 +0100

    Update telechargement_noaa.py

--- telechargement_noaa.py
+++ telechargement_noaa.py
@@ -4,12 +4,11 @@ import rasterio
 import pandas as pd
 
 # Download data from NOAA
-def download_noaa_data(date, time,nom_paquet):
+def download_noaa_data(date, heure_prevision,nom_paquet):
     url = f"https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?dir=%2Fgfs.{date}%2F{time}%2Fatmos&file=gfs.t06z.pgrb2.0p25.f000&all_var=on&all_lev=on"
-    print(url)
     response = requests.get(url)
     if response.status_code == 200:
-        with open(f"DONNEES_NOAA/donnees_noaa_{nom_paquet}_{date}T_{time}t_echeance_000", "wb") as file:
+        with open(f"DONNEES_NOAA/donnees_noaa_{nom_paquet}_{date}T_{heure_prevision}t_echeance_000", "wb") as file:
             file.write(response.content)
         print("GRIB file saved successfully.")
     else:
@@ -18,8 +17,16 @@ def download_noaa_data(date, time,nom_paquet):
 # Example usage
 #date = datetime.now().strftime("%Y%m%d")
 date = "20240221"
-
-time = "06"  # Example time, can be 0, 6, 12, 18
+#time = "06"  # Example time, can be 0, 6, 12, 18
 nom_paquet="HP1"
+heure=datetime.now().hour
+if heure <= 6:
+    heure_prevision = "00"
+elif heure <= 12:
+    heure_prevision = "06"
+elif heure <= 18:
+    heure_prevision = "12"
+else:
+    heure_prevision = "12"
 
-download_noaa_data(date, time, nom_paquet)
+download_noaa_data(date, heure_prevision, nom_paquet)
