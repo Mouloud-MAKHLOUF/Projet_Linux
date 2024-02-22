3f51cfcffe72319f239cda367a155e221f5ba38c
Salaheddine-ouikene <59568245+Salaheddine-ouikene@users.noreply.github.com>
2024-02-22 14:32:46 +0100

    Update telechargement_noaa.py

--- telechargement_noaa.py
+++ telechargement_noaa.py
@@ -8,7 +8,7 @@ def download_noaa_data(date, heure_prevision,nom_paquet):
     url = f"https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?dir=%2Fgfs.{date}%2F{heure_prevision}%2Fatmos&file=gfs.t06z.pgrb2.0p25.f000&all_var=on&all_lev=on"
     response = requests.get(url)
     if response.status_code == 200:
-        with open(f"DONNEES_NOAA/donnees_noaa_{nom_paquet}_{date}T_{heure_prevision}t_echeance_000", "wb") as file:
+        with open(f"DONNEES_NOAA/donnees_noaa_{date}T_{heure_prevision}t_echeance_000", "wb") as file:
             file.write(response.content)
         print("GRIB file saved successfully.")
     else:
@@ -18,7 +18,6 @@ def download_noaa_data(date, heure_prevision,nom_paquet):
 date = datetime.now().strftime("%Y%m%d")
 #date = "20240221"
 #time = "06"  # Example time, can be 0, 6, 12, 18
-nom_paquet="HP1"
 heure=datetime.now().hour
 if heure <= 6:
     heure_prevision = "00"
